aafd1bf07d07f9f98aff062487d21db4f0ee6508
Hifito <47784468+hifito@users.noreply.github.com>
2020-04-21 11:35:39 +0700

    Create Iterasi Fibonacci.c

---
+++ Iterasi Fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Masukkan jumlah deret fibonacci: ");
+    scanf("%d", &n);
+    fibo(n);
+    return 0;
+}
+
+void fibo(int n)
+{
+    int hasil;
+    static int fibo1;
+    static int fibo2;
+    fibo1=0;
+    fibo2=0;
+    for(int i=0;i<n;i++)
+    {
+        hasil=fibo1+fibo2;
+        printf("%d ", hasil);
+        if(hasil==0)
+            fibo2=1;
+        else
+        {
+            fibo1=fibo2;
+            fibo2=hasil;
+        }
+    }
+}
